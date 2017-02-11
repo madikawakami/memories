aad29881de9489c1199d356e593becf5482b0672
Madison Kawakami <madi.kawakami@gmail.com>
2017-02-10 15:12:51 -1000

    i tried

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ function flip(card) {
 		}, 1000);
 	}
 }
+
+
+var card = [];
+card[0] = '/assets/happy-face-sun-clipart-best-clipart-best-JTj5mT-clipart.jpeg';
+card[1] = '/assets/happy-rain-cloud-clip-art-iMKFdA-clipart.png';
+card[2] = '/assets/happy-cloud-clipart-happy-cloud-clipart_550-400.jpeg';
+card[3] = '/assets/flower-clipart17.jpg';
+card[4] = '/assets/happy-face-sun-clipart-best-clipart-best-JTj5mT-clipart.jpeg';
+card[5] = '/assets/happy-rain-cloud-clip-art-iMKFdA-clipart.png';
+card[6] = 'assets/happy-cloud-clipart-happy-cloud-clipart_550-400.jpeg';
+card[7] = 'assets/flower-clipart17.jpg';
+
+var clicks = 0; 
+var firstSelect; 
+var secondSelect;
+var match = 0;
+var cardBack = "/assets/valentines-day-heart-clip-art-33947.jpg";
+
